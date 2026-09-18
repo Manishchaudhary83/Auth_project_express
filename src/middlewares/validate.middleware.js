2de6bf6dc963ec646eda40b6f1e8ce0da0457b77
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 06:38:53 +0545

    imrove register valifdation and add login validation

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -12,32 +12,52 @@ async function validateResult(req, res, next) {
     next()
 }
 
-
+// =======================
 // Register validation rules
+// =======================
 const registerUserValidationRules = [
 
     // Full name validation
     body("fullName")
         .isString()
         .withMessage("Full name must be a string")
+        .trim()
         .notEmpty()
-        .withMessage("Full name is required"),
+        .withMessage("Full name is required")
+        .isLength({ min: 2, max: 100 })
+        .withMessage("Full name must be between 2 and 100 characters"),
 
     // Username validation
     body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
         .isString()
         .withMessage("Username must be a string")
         .isLength({ min: 3, max: 20 })
         .withMessage("Username must be between 3 and 20 characters"),
 
+
     // Email validation
     body("email")
+        .notEmpty()
+        .withMessage("Email is required")
         .isEmail()
         .withMessage("Invalid email address"),
 
+    // Phone
+    body("phone")
+        .trim()
+        .notEmpty()
+        .withMessage("Phone number is required")
+        .isMobilePhone("any")
+        .withMessage("Invalid phone number"),
+
     // Password validation
     body("password")
-        .isLength({ min: 8 })
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8, max:128})
         .withMessage("Password must be at least 8 characters"),
 
     // Run validation result
@@ -45,6 +65,34 @@ const registerUserValidationRules = [
 ]
 
 
+
+// =======================
+//login validation rules
+// ========================
+const loginUserValidationRules = [
+
+    body("username")
+        .optional()
+        .trim(),
+
+    body("email")
+        .optional()
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage("Invalid email address"),
+
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required"),
+
+    validateResult
+]
+
+
+
+
+
 module.exports = {
-    registerUserValidationRules
+    registerUserValidationRules, loginUserValidationRules
 }
