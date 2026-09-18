92cdef7463b9096aee73e59e28f5418b3055f327
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 06:18:14 +0545

    create route api for register, login and logout

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const {registerUserController, loginUserController, logoutUserController } = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+const {registerUserValidationRules} = require("../middlewares/validate.middleware")
+
+const router = express.Router()
+
+//API
+router.post("/register", registerUserValidationRules, registerUserController)
+router.post("/login",  loginUserController)
+router.post("/logout", authMiddleware, logoutUserController)
+
+
+module.exports = router
