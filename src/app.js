c105001fb563117cf65f5c6ab027a16a7b4f12e3
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 06:20:30 +0545

    create prefix and handling error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express = require("express")
+const authRoutes = require("./routes/auth.routes")
 
 const app = express()
+
+//json middleware
 app.use(express.json())
 
+
+//prefix
+app.use("/api/auth", authRoutes)
+
+app.use((error, req, res, next) => {
+    res.status(error.status ?? 500).json({
+        message: error.message ?? "Intrenal server error"
+    })
+})
 
 module.exports = app
