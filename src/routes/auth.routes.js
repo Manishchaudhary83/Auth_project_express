15072e2105bdf94367f6cc4746a862f5461fa80c
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 07:37:44 +0545

    forgot and reset password route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 const express = require("express")
-const {registerUserController, loginUserController, logoutUserController } = require("../controllers/auth.controller")
+const {registerUserController, loginUserController, forgotPasswordController, resetPasswordController, logoutUserController } = require("../controllers/auth.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
-const {registerUserValidationRules, loginUserValidationRules} = require("../middlewares/validate.middleware")
+const {registerUserValidationRules, loginUserValidationRules, forgotPasswordValidationRules, resetPasswordValidationRules} = require("../middlewares/validate.middleware")
 
 const router = express.Router()
 
 //API
 router.post("/register", registerUserValidationRules, registerUserController)
 router.post("/login", loginUserValidationRules, loginUserController)
+router.post("/forgot-password", forgotPasswordValidationRules, forgotPasswordController)
+router.post( "/reset-password/:token", resetPasswordValidationRules, resetPasswordController)
 router.post("/logout", authMiddleware, logoutUserController)
 
 
