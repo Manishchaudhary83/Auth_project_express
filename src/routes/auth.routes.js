b9953100703f205f44961054897b4e548834307c
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 06:39:38 +0545

    apply login validation rules on routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const {registerUserController, loginUserController, logoutUserController } = require("../controllers/auth.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
-const {registerUserValidationRules} = require("../middlewares/validate.middleware")
+const {registerUserValidationRules, loginUserValidationRules} = require("../middlewares/validate.middleware")
 
 const router = express.Router()
 
 //API
 router.post("/register", registerUserValidationRules, registerUserController)
-router.post("/login",  loginUserController)
+router.post("/login", loginUserValidationRules, loginUserController)
 router.post("/logout", authMiddleware, logoutUserController)
 
 
