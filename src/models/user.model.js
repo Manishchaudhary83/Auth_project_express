0f476eb76058c2ccd333a8ee07d666c8eea9dec5
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-18 06:15:27 +0545

    create usermodel and schema

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    }
+ },{
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel
